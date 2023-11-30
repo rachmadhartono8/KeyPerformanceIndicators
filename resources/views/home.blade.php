01f468e0a6ceebca48cb1da5454d01d27574eff3
Rachmad Hartono <59315926+rachmadhartono8@users.noreply.github.com>
2023-11-30 20:35:19 +0700

    menu login deploy to github

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/home.blade.php -->
+
+{{-- @extends('adminlte::page')
+
+@section('content_header')
+    <h1>Selamat Datang di Halaman Utama</h1>
+@stop
+
+@section('content')
+    <p>Ini adalah halaman utama proyek Laravel Anda. Silakan tambahkan konten sesuai kebutuhan Anda.</p>
+@stop --}}
